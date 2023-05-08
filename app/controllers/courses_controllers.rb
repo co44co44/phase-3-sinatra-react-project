ba185dd4787a1fa34691b4535dd2474b19e1ca73
Maria Cristina Ordonez <crisordonez44@gmail.com>
2023-05-08 16:20:35 -0400

    add courses controllers

---
+++ app/controllers/courses_controllers.rb
@@ -0,0 +1,7 @@
+class CoursesController <  ApplicationController
+    #index route
+    get '/courses' do
+        "Im courses controller"
+      end
+
+end
---
+++ app/controllers/students_controllers.rb
@@ -0,0 +1,7 @@
+class StudentsController <  ApplicationController
+    # index route
+    get '/students' do
+        "Im students controller"
+      end
+    
+end
