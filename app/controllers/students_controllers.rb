3d83ac991bee5744c9ba7a765211476046870839
Maria Cristina Ordonez <crisordonez44@gmail.com>
2023-05-08 22:19:58 -0400

    include courses at student controllers for nested json

--- app/controllers/students_controllers.rb
+++ app/controllers/students_controllers.rb
@@ -2,7 +2,7 @@ class StudentsController <  ApplicationController
     # students index route
     get '/students' do
         students = Student.all
-        students.to_json
+        students.to_json(include: :courses)
     end
     
 end
