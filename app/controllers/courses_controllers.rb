7c5588bdb3db86e2e970f3f539d0d6bd2d284562
Maria Cristina Ordonez <crisordonez44@gmail.com>
2023-05-23 15:59:12 -0400

    add more work on controllers

--- app/controllers/courses_controllers.rb
+++ app/controllers/courses_controllers.rb
@@ -1,10 +1,12 @@
 class CoursesController <  ApplicationController
-    # index courses route
+  
+  # index courses route: show all courses
     get '/courses' do
       courses = Course.all
       courses.to_json
     end
 
+  #adds course 
     post '/courses' do
       # binding.pry
       course = Course.create(
@@ -17,10 +19,24 @@ class CoursesController <  ApplicationController
       course.to_json
     end
 
-    delete '/courses/:id' do
+  #updates course info
+    patch '/courses/:id' do
       course = Course.find(params[:id])
-      course.destroy
+      course.update(
+        name:params[:name],
+        description:params[:description],
+        days:params[:days],
+        image_url:params[:image_url]
+      )
       course.to_json
-    end
+  end 
+
+  #deletes a course
+    # delete '/courses/:id' do
+    #   course = Course.find(params[:id])
+    #   course.destroy
+    #   course.to_json
+    # end
+
 
 end
--- app/controllers/students_controllers.rb
+++ app/controllers/students_controllers.rb
@@ -1,24 +1,36 @@
 class StudentsController <  ApplicationController
-    # students index route
+    
+
+    # students index route: show all students
     get '/students' do
         students = Student.all
         students.to_json(include: :courses)
     end
     
-    # students show route
+    # students show one student
     get '/students/:id' do
         student = Student.find(params[:id])
         student.to_json(include: :courses)
     end
 
+#adds a new student 
     post '/students' do
         student = Student.create(params)
         student.to_json(include: :courses)
     end
 
-    delete '/students' do
-        country = Student.find_by(id: params[:id])
-        country.destroy
-        country.to_json
-    end
+# updates a student name
+    patch '/students/:id' do
+    student = Student.find(params[:id])
+    student.update(name: params[:name])
+    student.to_json
+end
+
+# deletes a student with they courses.
+
+    # delete '/students/:id' do
+    #     student = Student.find(params[:id])
+    #     student.destroy (include: :courses)
+    #     student.to_json
+    # end
 end
