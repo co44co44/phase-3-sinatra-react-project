f5c432635b67876e74e846c96d76915e5e9e8f45
Maria Cristina Ordonez <crisordonez44@gmail.com>
2023-05-30 17:15:13 -0400

    change get request

--- app/controllers/students_controllers.rb
+++ app/controllers/students_controllers.rb
@@ -3,34 +3,33 @@ class StudentsController <  ApplicationController
 
     # students index route: show all students
     get '/students' do
-        students = Student.all
-        students.to_json(include: :courses)
+        students = Student.all.to_json(include: :courses)
     end
     
     # students show one student
-    get '/students/:id' do
-        student = Student.find(params[:id])
-        student.to_json(include: :courses)
-    end
+    # get '/students/:id' do
+    #     student = Student.find(params[:id])
+    #     student.to_json(include: :courses)
+    # end
 
-#adds a new student 
+    # adds a new student 
     post '/students' do
         student = Student.create(params)
         student.to_json(include: :courses)
     end
 
-# updates a student name
-    patch '/students/:id' do
-    student = Student.find(params[:id])
-    student.update(name: params[:name])
-    student.to_json
-end
+# # updates a student name
+#     patch '/students/:id' do
+#     student = Student.find(params[:id])
+#     student.update(name: params[:name])
+#     student.to_json
+# end
 
-# deletes a student with they courses.
+# deletes a student with their courses.
 
-    # delete '/students/:id' do
-    #     student = Student.find(params[:id])
-    #     student.destroy (include: :courses)
-    #     student.to_json
-    # end
+    delete '/students/:id' do
+        student = Student.find(params[:id])
+        student.destroy 
+        student.to_json
+    end
 end
